f87aaa1fa611a010fed524602cbe3b73ddb5d5dd
Pedro760 <pedrolima4680@gmail.com>
2022-01-26 09:15:21 -0300

    Selecionando um elemento

--- lib/pages/moedas_page.dart
+++ lib/pages/moedas_page.dart
@@ -1,28 +1,71 @@
+import 'package:app/models/moeda.dart';
 import 'package:app/repositories/moeda_repositoty.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class MoedasPage extends StatelessWidget {
-  const MoedasPage({Key? key}) : super(key: key);
+class MoedasPage extends StatefulWidget {
+  MoedasPage({Key? key}) : super(key: key);
+
+  @override
+  State<MoedasPage> createState() => _MoedasPageState();
+}
+
+class _MoedasPageState extends State<MoedasPage> {
+  final tabela = MoedaRepositoty.tabela;
+
+  NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+
+  List<Moeda> selecionadas = [];
 
   @override
   Widget build(BuildContext context) {
-    final tabela = MoedaRepositoty.tabela;
-
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Cripto Moedas'),
-        ),
-        body: ListView.separated(
-          itemBuilder: (BuildContext context, int moeda) {
-            return ListTile(
-              leading: Image.asset(tabela[moeda].icone),
-              title: Text(tabela[moeda].nome),
-              trailing: Text(tabela[moeda].preco.toString()),
-            );
-          },
-          padding: EdgeInsets.all(16),
-          separatorBuilder: (_, ___) => Divider(),
-          itemCount: tabela.length,
-        ));
+      appBar: AppBar(
+        title: Text('Cripto Moedas'),
+      ),
+      body: ListView.separated(
+        itemBuilder: (BuildContext context, int moeda) {
+          return ListTile(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(12),
+              ),
+            ),
+            leading: (selecionadas.contains(tabela[moeda]))
+                ? CircleAvatar(
+                    child: Icon(Icons.check),
+                  )
+                : SizedBox(
+                    child: Image.asset(tabela[moeda].icone),
+                    width: 40,
+                  ),
+            title: Text(
+              tabela[moeda].nome,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            trailing: Text(
+              real.format(tabela[moeda].preco),
+            ),
+            selected: selecionadas.contains(tabela[moeda]),
+            selectedTileColor: Colors.indigo[50],
+            onLongPress: () {
+              setState(() {
+                (selecionadas.contains(tabela[moeda]))
+                    ? selecionadas.remove(tabela[moeda])
+                    : selecionadas.add(tabela[moeda]);
+              });
+            },
+          );
+        },
+        padding: EdgeInsets.all(16),
+        separatorBuilder: (_, ___) => Divider(),
+        itemCount: tabela.length,
+      ),
+    );
   }
 }
+
+//16:52
